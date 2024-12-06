5d0224971b47780e34ba31cd97eb5ca3e777229c
Inga355 <albi1303@gmail.com>
2024-12-06 08:38:20 +0100

    improve card item html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -14,22 +14,22 @@ def get_animal_facts(data):
         output += '<li class="cards__item">'
         try:
             animal_name = animal["name"]
-            output += f"Name: {animal_name}<br>"
+            output += f"<strong>Name:</strong> {animal_name}<br>"
         except KeyError:
             continue
         try:
             animal_diet = animal["characteristics"]["diet"]
-            output += f"Diet: {animal_diet}<br>"
+            output += f"<strong>Diet:</strong> {animal_diet}<br>"
         except KeyError:
             continue
         try:
             animal_location = animal["locations"][0]
-            output += f"Location: {animal_location}<br>"
+            output += f"<strong>Location:</strong> {animal_location}<br>"
         except KeyError:
             continue
         try:
             animal_type = animal["characteristics"]["type"]
-            output += f"Type: {animal_type}<br>"
+            output += f"<strong>Type:</strong> {animal_type}<br>"
         except KeyError:
             continue
         output += "</li>"
